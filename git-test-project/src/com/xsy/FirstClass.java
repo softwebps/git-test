e42eed729b18d56e9fe9b0cc25abceda421ed038
xsy <xsy@qq.com>
2020-12-07 14:44:11 +0800

    把工程添加到本地仓库

---
+++ git-test-project/src/com/xsy/FirstClass.java
@@ -0,0 +1,11 @@
+package com.xsy;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("FirstClass");
+        System.out.println("FirstClass");
+        System.out.println("FirstClass");
+        System.out.println("FirstClass");
+        System.out.println("FirstClass");
+    }
+}
